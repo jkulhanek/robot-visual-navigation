45acb16841df0ac947a24476a16eb4d3f2fd9cea
Jonas Kulhanek <jonas.kulhanek@live.com>
2022-07-22 10:54:00 +0200

    Make wandb optional

--- python/train.py
+++ python/train.py
@@ -5,22 +5,24 @@ from deep_rl.core import AbstractTrainerWrapper
 from deep_rl.common.metrics import MetricHandlerBase
 import deep_rl.configuration as config
 from configuration import configuration
-import wandb
 
 
 class WandbMetricHandler(MetricHandlerBase):
     def __init__(self, *args, **kwargs):
         super(WandbMetricHandler, self).__init__("wandb", *args, **kwargs)
+
+        import wandb
+        self._wandb = wandb
         wandb.init()
 
     def collect(self, collection, time, mode='train'):
-        wandb.log(dict(collection), step=time)
-        pass
+        self._wandb.log(dict(collection), step=time)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("train")
     parser.add_argument("trainer")
+    parser.add_argument("--no-wandb", dest="wandb", action="store_false", help="Use wandb for logging.")
     args = parser.parse_args()
 
     deep_rl.configure(**configuration)
@@ -28,8 +30,9 @@ if __name__ == '__main__':
     config.logging.handlers = [
         "file",
         "matplotlib",
-        WandbMetricHandler
     ]
+    if args.wandb:
+        config.logging.handlers.append(WandbMetricHandler)
 
     import trainer as package
     trainer = make_trainer(args.trainer)
